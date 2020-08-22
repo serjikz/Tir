221da0ecc0a784901e0cc190c6fedaa626d9a16d
serjikz <zalukaev-sergey@yandex.ru>
2020-08-22 23:47:57 +0300

    panel class up

--- src/Panel.cpp
+++ src/Panel.cpp
@@ -13,14 +13,10 @@ Panel::Panel(rapidxml::xml_node<>* settings)
 }
 
 void Panel::draw() {
-	/*assert(_wheel);
-	FPoint textureCenter = FPoint(_wheel->getBitmapRect().Width() / 2, _wheel->getBitmapRect().Height() / 2 );
 	Render::device.PushMatrix();
 	Render::device.MatrixTranslate(_x, _y, 0);
-	Render::device.MatrixRotate(math::Vector3(0, 0, 1), _angle);
-	Render::device.MatrixTranslate(-textureCenter.x, -textureCenter.y, 0);
-	_wheel->Draw();
-	Render::device.PopMatrix();*/
+	_tex->Draw();
+	Render::device.PopMatrix();
 }
 
 void Panel::update(float dt) {
