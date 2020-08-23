55c45dd117fb1f1eb4ad499a0e12a00654aa6b8d
serjikz <zalukaev-sergey@yandex.ru>
2020-08-23 23:37:10 +0300

    scorepanel draw

--- src/ScorePanel.cpp
+++ src/ScorePanel.cpp
@@ -5,6 +5,11 @@ ScorePanel::ScorePanel(rapidxml::xml_node<>* settings)
 	:Panel(settings)
 {
 	_textureCenter = FPoint(_tex->getBitmapRect().Width() / 2.f, _tex->getBitmapRect().Height() / 2.f);
+	rapidxml::xml_node<>* backSideRectSettings = settings->first_node("backSideRect");
+	float x = Xml::GetFloatAttributeOrDef(backSideRectSettings, "x", 0.f);
+	float y = Xml::GetFloatAttributeOrDef(backSideRectSettings, "y", 0.f);
+	float scale = Xml::GetFloatAttributeOrDef(backSideRectSettings, "scale", 1.f);
+	_backSideRect = IRect(x, y, scale * _textureCenter.x, scale * _textureCenter.y);
 }
 
 void ScorePanel::draw() {
@@ -13,7 +18,14 @@ void ScorePanel::draw() {
 		Render::device.PushMatrix();
 		Render::device.MatrixTranslate(_x, _y, 0);
 		Render::device.MatrixScale(_splineVal, _splineVal, 1.f);
-		Render::device.MatrixTranslate(-_textureCenter.x, -_textureCenter.y, 0);
+		Render::device.MatrixTranslate(-_textureCenter.x, -_textureCenter.y, 0);	
+		Render::BeginAlphaMul(0.7f *_t);
+		Render::device.SetTexturing(false);
+		Render::BeginColor(Color::BLACK);
+		Render::DrawRect(_backSideRect);
+		Render::EndColor();
+		Render::EndAlphaMul();
+		Render::device.SetTexturing(true);
 		_tex->Draw();
 		Render::device.PopMatrix();
 		Render::EndAlphaMul();
--- src/ScorePanel.h
+++ src/ScorePanel.h
@@ -5,6 +5,7 @@ class ScorePanel : public Panel
 {
 private:	
 	FPoint _textureCenter;
+	IRect _backSideRect;
 public:
 	ScorePanel(rapidxml::xml_node<>* settings);
 	typedef boost::shared_ptr<ScorePanel> HardPtr;
